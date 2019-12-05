63894872bd2bf548d92f4234fb06a86e0b8101b2
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-05 02:31:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,16 @@ require "yaml"
 
 
 def load_library(filepath)
+  # returns a hash
+  # has two keys, 'get_meaning' and 'get_emoticon' (FAILED - 1)
+  # the keys 'get_meaning' and 'get_emoticon' point to inner hashes 
+  # the keys inside the 'get_meaning' hash are the Japanese emoticons 
+  # the emoticon keys inside the 'get_meaning' hash point to their meanings 
+  # the keys inside the 'get_emoticon' hash are the English emoticons 
+  # the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents 
+  
   dictionary = YAML.load_file(filepath)
+  meaning = dictionary.map 
   
 end
 
